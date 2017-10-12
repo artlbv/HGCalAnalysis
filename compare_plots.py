c50c278205d4f46a0c916f57b15ad017eacd8864
Artur Lobanov <artur.lobanov@cern.ch>
2017-10-12 17:01:32 +0200

    Overlay plots script

---
+++ compare_plots.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import os, sys
+import ROOT
+import phase2tdrStyle
+
+
+indir = "/home/llr/cms/lobanov/HGCAL/reco/clustering/flat_ntuple/CMSSW_9_3_2/src/RecoNtuples/HGCalAnalysis/test/ntuples/"
+
+samples = {}
+samples["1_sig"] = ("eleIDntuple_ele15_ele.root", "Ele, 15 GeV, no PU",ROOT.kRed+2)
+samples["2_sig"] = ("hgcalNtuple_ele15_932_PU140_n1000_helper_ele.root", "Ele, 15 GeV, PU140",ROOT.kMagenta+2)
+samples["3_bkg"] = ("eleIDntuple_qcd_n1000_ele.root", "QCD",ROOT.kCyan+2)
+samples["4_bkg"] = ("eleIDntuple_pi25_n1000_ele.root", "Pi, 25 GeV, no PU",ROOT.kYellow+2)
+
+histos = {samp:[] for samp in samples}
+hists = {}
+
+for sample in sorted(samples):
+
+    fname, title, color = samples[sample]
+
+    print fname, title
+
+    tfile = ROOT.TFile(indir + fname)
+
+    for key in tfile.GetListOfKeys():
+        obj = tfile.Get(key.GetName())
+        if obj.ClassName() != "TH1F": continue
+
+        ## Copy hist to storage
+        hname = sample + "_" + obj.GetName()
+        hist = obj.Clone(hname)
+        hist.SetDirectory(0)
+
+        hist.SetTitle(title)
+        hist.GetXaxis().SetTitle(obj.GetName().replace("ele_",""))
+
+        hist.SetLineColor(color)
+        hist.SetFillColorAlpha(color,0.4)
+        hist.SetMarkerColor(color)
+
+        histos[sample].append(hist)
+
+        if obj.GetName() in hists:
+            hists[obj.GetName()].append(hist)
+        else:
+            hists[obj.GetName()] = [hist]
+
+    tfile.Close()
+
+#outdir = "/eos/user/a/alobanov/www/HGCAL/reco/eleID/compare_ele_ID_vars/"
+outdir = "compare_vars/"
+
+if not os.path.exists(outdir): os.makedirs(outdir)
+
+for hname in hists:
+
+    cname = "c" + hname
+    canv = phase2tdrStyle.setCanvas()
+    canv.SetName(cname)
+    canv.SetTitle(hname)
+
+    for i, hist in enumerate(hists[hname]):
+
+        if i == 0: hist.DrawNormalized()
+        else: hist.DrawNormalized("same")
+
+    leg = canv.BuildLegend()
+    leg.Draw()
+    leg.SetX1(0.65)
+    leg.SetX2(0.9)
+    leg.SetY1(0.7)
+    leg.SetY2(0.9)
+
+    phase2tdrStyle.drawCMS(True)
+    phase2tdrStyle.drawEnPu()
+
+    canv.Update()
+    canv.Draw()
+    canv.SetLogy()
+
+    #q = raw_input("q")
+
+    canv.SaveAs(outdir + cname + ".pdf")
+    canv.SaveAs(outdir + cname + ".png")
